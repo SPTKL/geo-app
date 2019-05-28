dcd86ea5d227c605dc2ea62ee7834d2b45310bc7
Baiyue Cao <caobaiyue@gmail.com>
2019-05-27 21:00:23 -0400

    finish basic functionalities

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ import io
 from geocode import geocode
 from spatial_options import spatial_options
 
+import json
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-
+import redis
+import uuid
+from flask import Flask, request, Response
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.config.supress_callback_exceptions = True
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 app.layout = html.Div([
 
@@ -51,16 +57,18 @@ app.layout = html.Div([
     html.Div(id='output-data-upload'),
 ])
 
-
 def parse_contents(contents, opt, filename, date):
-    content_type, content_string = contents.split(',')
+    r.flushdb()
+    session_id = str(uuid.uuid4())
 
+    content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
+
     try:
         if 'csv' in filename:
             # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            df_sample = df[0:5]
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded))
@@ -72,15 +80,17 @@ def parse_contents(contents, opt, filename, date):
     DATA = []
     for i in df.to_dict('records'):
         results = geocode(i['house_number'],i['street_name'],
-                        i['borough'], mode='api', columns=opt)
+                        i['borough_code'], mode='api', columns=opt)
         DATA.append(results)
+    r.set(session_id, json.dumps(DATA))
 
     return html.Div([
+        html.Div(session_id, id='session-id', style={'display': 'none'}),
         html.H5(f'File name: {filename}'),
         html.H6(f'Last edit: {datetime.datetime.fromtimestamp(date)}'),
 
         dash_table.DataTable(
-            data=DATA,
+            data=DATA[0:5],
             columns=[{'name': i, 'id': i} for i in opt],
             style_table={'overflowX': 'scroll',
                          'maxHeight': '300px',
@@ -92,6 +102,16 @@ def parse_contents(contents, opt, filename, date):
 
         html.Hr(),  # horizontal line
 
+        html.Div(children=[
+                html.A(id = 'download-link', children=[
+                        html.Button('Download',
+                            id='submit',
+                            type='submit',
+                            className='button-primary')
+                        ])
+                ], style={'padding': '20px 20px 20px 20px'}),
+
+        html.Hr(),
         # For debugging, display the raw contents provided by the web browser
         html.Div('Raw Content'),
         html.Pre(contents[0:200] + '...', style={
@@ -113,6 +133,24 @@ def update_output(list_of_contents, opt, list_of_names, list_of_dates):
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
+@app.callback(
+    Output('download-link', 'href'),
+    [Input('session-id', 'children')])
+def get_data(session_id):
+    return f'/download.csv?session_id={session_id}'
+
+@server.route('/download.csv')
+def download_csv():
+    session_id = request.args.get('session_id')
+    def generate():
+        i = 0
+        for row in json.loads(r.get(session_id)):
+            print(row)
+            if i == 0:
+                yield ','.join(str(i) for i in row.keys()) + '\n'
+                i += 1
+            yield ','.join(str(x) for x in row.values()) + '\n'
+    return Response(generate(), mimetype = 'text/csv')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- geocode.py
+++ geocode.py
@@ -2,8 +2,8 @@
 import requests
 import json
 
-def geocode(hnum, sname, boro, mode='api', columns=['Longitude', 'Latitude']):
-    url_prefix = 'http://api-geosupport.planninglabs.nyc:5000/1b?'
-    url = f'{url_prefix}house_number={hnum}&street_name={sname}&borough={boro}'
+def geocode(hnum, sname, boro, columns, mode='api'):
+    url_prefix = 'https://geosupport.planninglabs.nyc/1B?'
+    url = f'{url_prefix}house_number={hnum}&street_name={sname}&borough_code={boro}'
     content = json.loads(requests.get(url).content)
-    return {key:content['results'][key] for key in columns}
+    return {key:content[key] for key in columns}
